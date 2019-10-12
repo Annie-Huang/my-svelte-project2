1809de52c9c59d713a53f1fb9ab397f0e3a1c9e4
Annie-Huang <annie.hn@gmail.com>
2019-10-12 21:10:41 +1100

    Introduction - Styling

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,19 @@
     let src = 'tutorial/image.gif';
 </script>
 
+<style>
+    p {
+        color: purple;
+        font-family: 'Comic Sans MS';
+        font-size: 2em;
+    }
+</style>
+
 <h1>Hello {name.toUpperCase()}!</h1>
 <img src={src} alt="A man dances.">
 <img src={src} alt="{name} dances.">
 
 <!--Shorthand attributes-->
 <img {src} alt="A man dances.">
+
+<p>This is a paragraph.</p>
