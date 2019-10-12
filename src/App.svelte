12dcd90bd5a118f0748868cfd6b26a17487a5fae
Annie-Huang <annie.hn@gmail.com>
2019-10-12 21:25:17 +1100

    Introduction - HTML tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,10 @@
+<!--From Introduction/HTML tags to -->
 <script>
-    import Nested from './Nested.svelte';
-
-	let name = 'world';
-    let src = 'tutorial/image.gif';
+    let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
-<style>
-    p {
-        color: purple;
-        font-family: 'Comic Sans MS';
-        font-size: 2em;
-    }
-</style>
-
-<h1>Hello {name.toUpperCase()}!</h1>
-<img src={src} alt="A man dances.">
-<img src={src} alt="{name} dances.">
-
-<!--Shorthand attributes-->
-<img {src} alt="A man dances.">
-
-<p>This is a paragraph.</p>
-<Nested/>
+<!--Svelte doesn't perform any sanitization of the expression inside {@html ...} before it gets inserted into the DOM.
+    In other words, if you use this feature it's critical that you manually escape HTML that comes from sources
+    you don't trust, otherwise you risk exposing your users to XSS attacks.-->
+<p>{string}</p>
+<p>{@html string}</p>
---
+++ src/App01.svelte
@@ -0,0 +1,30 @@
+<!--From Introduction/Adding data to Introduction/Nested components-->
+
+<script>
+    import Nested from './Nested.svelte';
+
+	let name = 'world';
+    let src = 'tutorial/image.gif';
+    let string = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
+<style>
+    p {
+        color: purple;
+        font-family: 'Comic Sans MS';
+        font-size: 2em;
+    }
+</style>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+<img src={src} alt="A man dances.">
+<img src={src} alt="{name} dances.">
+
+<!--Shorthand attributes-->
+<img {src} alt="A man dances.">
+
+<p>This is a paragraph.</p>
+<Nested/>
+
+<p>{string}</p>
+<p>{@html string}</p>
