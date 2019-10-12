cf5b1ac4feb5d9672895ef012c321fdd55be2684
Annie-Huang <annie.hn@gmail.com>
2019-10-12 22:38:15 +1100

    Reactivity - Assignments

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <!--From Introduction/HTML tags to -->
 <script>
     let string = `this string contains some <strong>HTML!!!</strong>`;
+
+    let count = 0;
+
+    function handleClick() {
+        // event handler code goes here
+        count += 1;
+    }
 </script>
 
 <!--Svelte doesn't perform any sanitization of the expression inside {@html ...} before it gets inserted into the DOM.
@@ -8,3 +15,7 @@
     you don't trust, otherwise you risk exposing your users to XSS attacks.-->
 <p>{string}</p>
 <p>{@html string}</p>
+
+<button on:click={handleClick}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
