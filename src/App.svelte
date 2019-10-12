8517fff1fb9106508f65a845bb50fd6ac52b879b
Annie-Huang <annie.hn@gmail.com>
2019-10-12 21:08:16 +1100

    Introduction - Dynamic attributes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
-	let name = 'world'
+	let name = 'world';
+    let src = 'tutorial/image.gif';
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<img src={src} alt="A man dances.">
+<img src={src} alt="{name} dances.">
+
+<!--Shorthand attributes-->
+<img {src} alt="A man dances.">
