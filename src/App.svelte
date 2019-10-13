11710dad9ec0076e3040cad3f0487a5b4ac8a7a3
Annie-Huang <annie.hn@gmail.com>
2019-10-13 13:12:18 +1100

    Events - Component events

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,17 @@
     function handleClick() {
         alert('no more alerts')
     }
+
+    import Inner from './Inner.svelte';
+
+    function handleMessage(event) {
+        alert(event.detail.text);
+    }
 </script>
 
 <button on:click|once={handleClick}>
     Click me
 </button>
+
+<br/>
+<Inner on:message={handleMessage}/>
---
+++ src/Inner.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    // createEventDispatcher must be called when the component is first instantiated — you can't do it later inside
+    // e.g. a setTimeout callback. This links dispatch to the component instance.
+    const dispatch = createEventDispatcher();
+
+    function sayHello() {
+        dispatch('message', {
+            text: 'Hello!'
+        });
+    }
+</script>
+
+<button on:click={sayHello}>
+    Click to say hello
+</button>
