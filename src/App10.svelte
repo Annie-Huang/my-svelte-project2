d3e054813def410430004de2d5cc17046a739b1e
Annie-Huang <annie.hn@gmail.com>
2019-10-13 21:07:51 +1100

    Bindings - media elements

---
+++ src/App10.svelte
@@ -0,0 +1,52 @@
+<!--From Bindings/Each block bindings to Bindings/Each block bindings-->
+<script>
+    let todos = [
+        { done: false, text: 'finish Svelte tutorial' },
+        { done: false, text: 'build an app' },
+        { done: false, text: 'world domination' }
+    ];
+
+    function add() {
+        todos = todos.concat({ done: false, text: '' });
+    }
+
+    function clear() {
+        todos = todos.filter(t => !t.done);
+    }
+
+    $: remaining = todos.filter(t => !t.done).length;
+</script>
+
+<style>
+    .done {
+        opacity: 0.4;
+    }
+</style>
+
+<h1>Todos</h1>
+
+<!--Note that interacting with these <input> elements will mutate the array.
+    If you prefer to work with immutable data, you should avoid these bindings and use event handlers instead.-->
+{#each todos as todo}
+    <div class:done={todo.done}>
+        <input
+                type=checkbox
+                bind:checked={todo.done}
+        >
+
+        <input
+                placeholder="What needs to be done?"
+                bind:value={todo.text}
+        >
+    </div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+    Add new
+</button>
+
+<button on:click={clear}>
+    Clear completed
+</button>
