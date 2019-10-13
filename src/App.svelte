d7c1231954222a7eb30d74320121f4c2f99cad53
Annie-Huang <annie.hn@gmail.com>
2019-10-13 11:09:50 +1100

    Logic - Else blocks

--- src/App.svelte
+++ src/App.svelte
@@ -33,3 +33,18 @@
             Log in
         </button>
 {/if}
+
+<br>
+
+<!-- A # character always indicates a block opening tag. A / character always indicates a block closing tag.
+     A : character, as in {:else}, indicates a block continuation tag.
+     Don't worry — you've already learned almost all the syntax Svelte adds to HTML.-->
+{#if user.loggedIn}
+        <button on:click={toggle}>
+            Log out
+        </button>
+{:else}
+        <button on:click={toggle}>
+            Log in
+        </button>
+{/if}
