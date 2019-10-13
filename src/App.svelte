7f482a670ec26e04d71c98348b4a3c60071cbb64
Annie-Huang <annie.hn@gmail.com>
2019-10-13 11:04:25 +1100

    Logic - If blocks

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,28 @@
         speed: 'blazing',
         website: 'https://svelte.dev'
     };
+
+    let user = { loggedIn: false };
+
+    function toggle() {
+        user.loggedIn = !user.loggedIn;
+    }
 </script>
 
+<!-- Conversely, if you need to reference all the props that were passed into a component,
+     including ones that weren't declared with export, you can do so by accessing $$props directly.
+     It's not generally recommended, as it's difficult for Svelte to optimise, but it's useful in rare cases.-->
 <Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>
 <Info {...pkg}/>
+
+{#if user.loggedIn}
+        <button on:click={toggle}>
+            Log out
+        </button>
+{/if}
+
+{#if !user.loggedIn}
+        <button on:click={toggle}>
+            Log in
+        </button>
+{/if}
