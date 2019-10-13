6b0c0a015bcd1e8da36423da807f88d39012abdf
Annie-Huang <annie.hn@gmail.com>
2019-10-13 16:40:50 +1100

    Bindings - Text inputs

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,17 @@
     function handleClick() {
         alert('clicked');
     }
+
+    let name = 'world';
 </script>
 
-<FancyButton on:click={handleClick}/>
+<FancyButton on:click={handleClick} />
+
+<br />
+<!--As a general rule, data flow in Svelte is top down — a parent component can set props on a child component,
+    and a component can set attributes on an element, but not the other way around.
+    we can use the bind:value directive:
+    This means that not only will changes to the value of name update the input value, but changes to the input value will update name.-->
+<input bind:value={name}>
+
+<h1>Hello {name}!</h1>
