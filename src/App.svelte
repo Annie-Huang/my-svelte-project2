492678dd8c43ce9ef611222580435b40b0e96914
Annie-Huang <annie.hn@gmail.com>
2019-10-15 22:20:38 +1100

    Stores - Auto-subscriptions

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,31 @@
 <!--From Stores/Writable stores to-->
 <script>
+    import { onDestroy } from 'svelte';
     import { count } from './stores.js';
     import Incrementer from './Incrementer.svelte';
     import Decrementer from './Decrementer.svelte';
     import Resetter from './Resetter.svelte';
 
-    let count_value;
+    // let count_value;
+    //
+    // const unsubscribe = count.subscribe(value => {
+    //     count_value = value;
+    // });
+    //
+    // // The app in the previous example works, but there's a subtle bug — the unsubscribe function never gets called.
+    // // If the component was instantiated and destroyed many times, this would result in a memory leak.
+    // onDestroy(unsubscribe);
 
-    const unsubscribe = count.subscribe(value => {
-        count_value = value;
-    });
+    // You're not limited to using $count inside the markup, either — you can use it anywhere in the <script> as well,
+    // such as in event handlers or reactive declarations.
+    // Any name beginning with $ is assumed to refer to a store value.
+    // It's effectively a reserved character — Svelte will prevent you from declaring your own variables with a $ prefix.
 </script>
 
-<h1>The count is {count_value}</h1>
+<!--<h1>The count is {count_value}</h1>-->
+
+<!--Auto-subscription only works with store variables that are declared (or imported) at the top-level scope of a component.-->
+<h1>The count is {$count}</h1>
 
 <Incrementer/>
 <Decrementer/>
