2bc7c8de60d88f7be10cef24ee31bb6f799bbe1d
Annie-Huang <annie.hn@gmail.com>
2019-10-16 20:59:54 +1100

    Motion - Spring

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,41 @@
-<!--From Motion/Tweened to-->
+<!--From Motion/Spring to-->
 <script>
-    // import { writable } from 'svelte/store';
-    //
-    // const progress = writable(0);
+    // The spring function is an alternative to tweened that often works better for values that are frequently changing.
+    import { writable, spring } from 'svelte/store';
 
-    import { tweened } from 'svelte/motion';
+    // let coords = writable({ x: 50, y: 50 });
+    // let size = writable(10);
 
-    // const progress = tweened(0);
+    // let coords = spring({ x: 50, y: 50 });
+    let size = spring(10);
 
-    // The svelte/easing module contains the , or you can supply your own p => t function where p and t are both values between 0 and 1.
-    import { cubicOut } from 'svelte/easing';
-
-    const progress = tweened(0, {
-        duration: 400,
-        easing: cubicOut
+    let coords = spring({ x: 50, y: 50 }, {
+        stiffness: 0.1,
+        damping: 0.25
     });
 </script>
 
 <style>
-    progress {
-        display: block;
-        width: 100%;
-    }
+    svg { width: 100%; height: 100%; margin: -8px; }
+    circle { fill: #ff3e00 }
 </style>
 
-<progress value={$progress}></progress>
+<div style="position: absolute; right: 1em;">
+    <label>
+        <h3>stiffness ({coords.stiffness})</h3>
+        <input bind:value={coords.stiffness} type="range" min="0" max="1" step="0.01">
+    </label>
 
-<button on:click="{() => progress.set(0)}">
-    0%
-</button>
+    <label>
+        <h3>damping ({coords.damping})</h3>
+        <input bind:value={coords.damping} type="range" min="0" max="1" step="0.01">
+    </label>
+</div>
 
-<button on:click="{() => progress.set(0.25)}">
-    25%
-</button>
-
-<button on:click="{() => progress.set(0.5)}">
-    50%
-</button>
-
-<button on:click="{() => progress.set(0.75)}">
-    75%
-</button>
-
-<button on:click="{() => progress.set(1)}">
-    100%
-</button>
+<svg
+        on:mousemove="{e => coords.set({ x: e.clientX, y: e.clientY })}"
+        on:mousedown="{() => size.set(30)}"
+        on:mouseup="{() => size.set(10)}"
+>
+    <circle cx={$coords.x} cy={$coords.y} r={$size}/>
+</svg>
---
+++ src/App21.svelte
@@ -0,0 +1,47 @@
+<!--From Motion/Tweened to Motion/Tweened-->
+<script>
+    // import { writable } from 'svelte/store';
+    //
+    // const progress = writable(0);
+
+    import { tweened } from 'svelte/motion';
+
+    // const progress = tweened(0);
+
+    // The svelte/easing module contains the , or you can supply your own p => t function where p and t are both values between 0 and 1.
+    import { cubicOut } from 'svelte/easing';
+
+    const progress = tweened(0, {
+        duration: 400,
+        easing: cubicOut
+    });
+</script>
+
+<style>
+    progress {
+        display: block;
+        width: 100%;
+    }
+</style>
+
+<progress value={$progress}></progress>
+
+<button on:click="{() => progress.set(0)}">
+    0%
+</button>
+
+<button on:click="{() => progress.set(0.25)}">
+    25%
+</button>
+
+<button on:click="{() => progress.set(0.5)}">
+    50%
+</button>
+
+<button on:click="{() => progress.set(0.75)}">
+    75%
+</button>
+
+<button on:click="{() => progress.set(1)}">
+    100%
+</button>
