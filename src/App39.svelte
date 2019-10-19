351a0d7b89fd96ccbc64428b4e315701f55c2efd
Annie-Huang <annie.hn@gmail.com>
2019-10-19 15:55:55 +1100

    Special elements - <svelte:options>

---
+++ src/App39.svelte
@@ -0,0 +1,6 @@
+<!--From Special elements/<svelte:head> to Special elements/<svelte:head>-->
+<svelte:head>
+    <link rel="stylesheet" href="assets/dark-theme.css">
+</svelte:head>
+
+<h1>Hello world!</h1>
---
+++ src/Todo.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { afterUpdate } from 'svelte';
+    import flash from './flash.js';
+
+    export let todo;
+    export let toggle;
+
+    let div;
+
+    afterUpdate(() => {
+        flash(div);
+    });
+</script>
+
+<style>
+    div {
+        cursor: pointer;
+        line-height: 1.5;
+    }
+</style>
+
+<!-- the text will flash red whenever
+     the `todo` object changes -->
+
+<!--You can shorten this to <svelte:options immutable/> if you prefer.-->
+<svelte:options immutable={true}/>
+
+<div bind:this={div} on:click>
+    {todo.done ? '👍': ''} {todo.text}
+</div>
