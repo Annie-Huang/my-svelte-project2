91d9be6693d8488ac92e521a246d2639620b3bd8
Annie-Huang <annie.hn@gmail.com>
2019-10-19 15:49:08 +1100

    Special elements - <svelte:head>

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,6 @@
-<!--From Special elements/<svelte:body> to-->
-<script>
-    let hereKitty = false;
+<!--From Special elements/<svelte:head> to-->
+<svelte:head>
+    <link rel="stylesheet" href="assets/dark-theme.css">
+</svelte:head>
 
-    const handleMouseenter = () => hereKitty = true;
-    const handleMouseleave = () => hereKitty = false;
-</script>
-
-<style>
-    img {
-        position: absolute;
-        left: 0;
-        bottom: -60px;
-        transform: translate(-80%, 0) rotate(-30deg);
-        transform-origin: 100% 100%;
-        transition: transform 0.4s;
-    }
-
-    .curious {
-        transform: translate(-15%, 0) rotate(0deg);
-    }
-
-    :global(body) {
-        overflow: hidden;
-    }
-</style>
-
-<svelte:body
-        on:mouseenter={handleMouseenter}
-        on:mouseleave={handleMouseleave}
-/>
-
-<!-- creative commons BY-NC http://www.pngall.com/kitten-png/download/7247 -->
-<img
-        class:curious={hereKitty}
-        alt="Kitten wants to know what's going on"
-        src="assets/kitten.png"
->
+<h1>Hello world!</h1>
